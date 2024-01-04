205229295ec03a82768ce82d47127b78b1e1a733
David <david.ndiba@student.moringaschool.com>
2024-01-04 04:11:56 +0300

    started stylings

--- d2s/src/pages/Login.js
+++ d2s/src/pages/Login.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-import  PrimaryButton  from './PrimaryButton';
+import { Link } from 'react-router-dom';
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import backgroundImage from '../assets/image1.png';
-import image from "../assets/dhis21.png";
-// import image21 from "../assets/image21.png";
-// import image22 from "../assets/image22.png";
-// import image23 from "../assets/image23.png";
-import "./Login.css"
+import image from '../assets/dhis21.png';
+import './Login.css';
 
-function Login  () {
+function Login() {
   return (
     <div className="login-container">
       <div className="row">
@@ -18,57 +14,54 @@ function Login  () {
         <div className="col-md-5 image-text-container">
           <div className="d-flex flex-column align-items-center justify-content-center h-100">
             <img src={backgroundImage} alt="Background" className="img-fluid mb-6" />
-            
             <div className="text">
               <p className="body-large">
-                The comprehensive and user-friendly platform that facilitates seamless data exchange between DHIS2
-                instances, as well as other data collection tools like Commcare, Kobo Toolbox, and ODK. Its interface
-                facilitates both one-time and scheduled data exchange.
+                The comprehensive and user-friendly platform that facilitates seamless data exchange between DHIS2 instances, as well as other data collection tools like Commcare, Kobo Toolbox, and ODK. Its interface facilitates both one-time and scheduled data exchange.
               </p>
               <div className="display-large">Welcome to D2S Stream.</div>
               <p className="text-wrapper">D2S: Automating Data Exchange for DHIS2 and Beyond</p>
               <div className="div">
                 <div className="dhis-wrapper">
                   <img className="dhis dhis-image" alt="Dhis" src={image} />
-                  {/* <img className="dhis dhis-image1" alt="Dhis" src={image21} /> */}
                 </div>
-                {/* <img className="dhis dhis-image2" alt="Dhis" src={image22} /> */}
-                {/* <img className="dhis dhis-image3" alt="Dhis" src={image23} /> */}
               </div>
             </div>
           </div>
         </div>
 
         {/* Login Form Container */}
-        <div className="col-md-6 form-container">
-          <div className="d-flex align-items-center justify-content-center h-100">
+        <div className="col-md-2">
+          <div className="form-container">
             <div className="login-form">
-              {/* Integration of Frame component */}
-              <div className="frame">
-                <p className="div">Sign In to Your Account</p>
-                <div className="div-2">
-                  <input className="password" id="input-1" placeholder="Email Address" type="email" />
-                  <div className="div-wrapper">
-                    <label className="label" htmlFor="input-1">
-                      Password
-                    </label>
-                  </div>
-                </div>
-                <Link to="/overview"  className="outline-fill-default-instance" text="Sign In" />
-                <a href="#!" className="forgot-pass">Forgot password?</a>
-                <PrimaryButton
-                  className="primary-button-instance"
-                  text="Don’t have an account? Create one"
-                  textClassName="design-component-instance-node"
-                />
+              <div className="form-header">
+                <h2>Sign In to Your Account</h2>
               </div>
-              
+              <div className="form-input">
+                <label htmlFor="email">Email Address:</label>
+                <input id="email" type="email" placeholder="Enter your email" />
+              </div>
+              <div className="form-input">
+                <label htmlFor="password">Password:</label>
+                <input id="password" type="password" placeholder="Enter your password" />
+              </div>
+              <button type="button" className="sign-in-button">
+                Sign In
+              </button>
+              <div className="form-links">
+                <a href="#!" className="forgot-password">
+                  Forgot your password?
+                </a>
+                <span className="divider"> | </span>
+                <Link to="/signup" className="create-account">
+                  Don't have an account? Create one
+                </Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Login;
--- d2s/src/pages/PrimaryButton.js
+++ d2s/src/pages/PrimaryButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const PrimaryButton = ({ text }) => {
-  return <button className="primary-button">{text}</button>;
+  return <button className="primary-button">Don't have an account? Create one.</button>;
 };
 
 export default PrimaryButton;
